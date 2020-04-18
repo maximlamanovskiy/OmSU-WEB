c68cab08938f07423e63a31451a6aecbc6155639
Ламановский Максим <maximlamanovskiy@gmail.com>
2020-04-18 23:30:49 +0600

    Updated gitignore. Created base for spring app. Created model for user

---
+++ src/main/java/ru/omsu/imit/web_spring_kotlin/Main.kt
@@ -0,0 +1,13 @@
+package ru.omsu.imit.web_spring_kotlin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class Main
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Main::class.java, *args)
+}
+
+
---
+++ src/main/java/ru/omsu/imit/web_spring_kotlin/core/model/User.kt
@@ -0,0 +1,26 @@
+package ru.omsu.imit.web_spring_kotlin.core.model
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+
+@Entity
+@Table(name = "USERS")
+open class User(_id: String, _username: String, _password: String) {
+
+    @Id
+    private val id: String = _id
+
+    @Column(name = "username", nullable = false)
+    private val username: String = _username
+
+    @JsonIgnore
+    @Column(name = "password", nullable = false)
+    private val password: String = _password
+
+    constructor(_username: String, _password: String) : this(UUID.randomUUID().toString(), _username, _password)
+}
