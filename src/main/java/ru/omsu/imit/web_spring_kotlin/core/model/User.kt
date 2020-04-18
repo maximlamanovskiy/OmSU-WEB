5d0010968b53ca623907898e8134a06714164bc9
Ламановский Максим <maximlamanovskiy@gmail.com>
2020-04-19 01:17:04 +0600

    Updated user model

--- src/main/java/ru/omsu/imit/web_spring_kotlin/core/model/User.kt
+++ src/main/java/ru/omsu/imit/web_spring_kotlin/core/model/User.kt
@@ -2,10 +2,15 @@ package ru.omsu.imit.web_spring_kotlin.core.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import java.util.UUID
-import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.Table
+import javax.persistence.Id
+import javax.persistence.Column
+import javax.persistence.ManyToMany
+import javax.persistence.FetchType
+import javax.persistence.CascadeType
+import javax.persistence.JoinTable
+import javax.persistence.JoinColumn
 
 
 @Entity
@@ -13,7 +18,13 @@ import javax.persistence.Table
 open class User constructor(
         @Id open val id: String,
         @Column(name = "username", nullable = false) open val username: String,
-        @JsonIgnore @Column(name = "password", nullable = false) open val password: String
+        @JsonIgnore @Column(name = "password", nullable = false) open val password: String,
+        @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+        @JoinTable(
+                name = "ROLE_FOR_USER",
+                joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
+                inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")])
+        val roles: List<Role> = mutableListOf()
 ) {
     constructor(_username: String, _password: String) : this(UUID.randomUUID().toString(), _username, _password)
     constructor(): this("", "", "")
