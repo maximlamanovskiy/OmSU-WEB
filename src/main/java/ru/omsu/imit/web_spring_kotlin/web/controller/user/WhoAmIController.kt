7ef17e81694c304cf480d2e85535c321e375933f
Maxim Lamanovsky <maxim.lamanovsky@7bits.it>
2020-04-22 01:22:17 +0600

    Created more page. Started working on who am i controller

---
+++ src/main/java/ru/omsu/imit/web_spring_kotlin/web/controller/user/WhoAmIController.kt
@@ -0,0 +1,16 @@
+package ru.omsu.imit.web_spring_kotlin.web.controller.user
+
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
+@RequestMapping("/whoAmI")
+class WhoAmIController {
+    @RequestMapping(method = [RequestMethod.GET], produces = [MediaType.TEXT_HTML_VALUE])
+    fun getUserInfo(model: Model) {
+
+    }
+}
